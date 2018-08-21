fada764a5cfa6f93bb7794cee945aa0b68de8082
Akash Hamirwasia <akash.hamirwasia@gmail.com>
2018-08-21 22:45:28 +0530

    Added the about page

---
+++ src/components/CardProfile.vue
@@ -0,0 +1,56 @@
+<template>
+  <card>
+    <div class="image" :style='{backgroundImage: `url(${image})`}' />
+    <h3>{{ title }}</h3>
+    <p>
+      <slot />
+    </p>
+  </card>
+</template>
+
+<script>
+import card from '@/components/Card'
+
+export default {
+  name: 'CardProfile',
+  props: ['title', 'image'],
+  components: {
+    card
+  }
+}
+</script>
+
+<style scoped>
+
+  .card {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    text-align: center;
+  }
+
+  .image {
+    width: 130px;
+    height: 130px;
+    border-radius: 100%;
+    background-size: cover;
+    background-position: center;
+    border: 5px solid #ffffff;
+
+    margin-top: -65px;
+  }
+
+  h3 {
+    font-weight: 700;
+    margin-top: 5px;
+    font-size: 1.6rem;
+    line-height: 1.2em;
+  }
+
+  p {
+    font-size: 1.1rem;
+    margin-top: 10px;
+    margin-bottom: 10px;
+  }
+
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,18 +1,84 @@
 <template>
   <div class="about">
     <hero>
-      <h1 style="color: white">Coming Soon</h1>
+      <Header />
+      <h2>About</h2>
+      <h3>The backbone of News Detector</h3>
     </hero>
+
+    <section class="team">
+      <card-profile
+        title="Neel Kamath"
+        :image="require('../assets/neel.jpeg')"
+      >
+        Worked on something
+      </card-profile>
+      <card-profile
+        title="Akash Hamirwasia"
+        :image="require('../assets/akash.jpg')"
+      >
+        Worked on something
+      </card-profile>
+      <card-profile
+        title="Siri R J"
+        :image="require('../assets/siri.jpeg')"
+      >
+        Worked on something
+      </card-profile>
+    </section>
+
+    <section class="pattern final">
+      <h2>Sources we used</h2>
+      <h3>Coming Soon</h3>
+    </section>
+
   </div>
 </template>
 
 <script>
 import hero from '@/components/Hero'
+import Header from '@/components/Header'
+import CardProfile from '@/components/CardProfile'
 
 export default {
   name: 'about',
   components: {
-    hero
+    hero,
+    Header,
+    'card-profile': CardProfile
   }
 }
 </script>
+
+<style scoped>
+
+  section.team {
+    display: flex;
+    flex-direction: column;
+    top: -95px;
+    padding: 0 20px;
+  }
+  section.team .card {
+    margin: 50px 15px 15px;
+  }
+
+  @media screen and (min-width: 650px) {
+    section.team { 
+      flex-direction: row;
+      flex-wrap: nowrap;
+    }
+
+    section.team .card {
+      width: 33%;
+    }
+
+  }
+
+  @media screen and (min-width: 950px) {
+    section.team {
+      padding: 0 80px;
+    }
+
+  }
+
+</style>
