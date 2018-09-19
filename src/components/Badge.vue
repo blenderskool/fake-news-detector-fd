aa080daaa5e696b9aca0e5583197e0c95ac91368
Akash Hamirwasia <akash.hamirwasia@gmail.com>
2018-09-19 19:07:21 +0530

    Added flowing wave style animation

--- src/components/Badge.vue
+++ src/components/Badge.vue
@@ -6,7 +6,7 @@
   }">
     <h3>{{ type }}</h3>
     <div class="level">
-      <span class="fill" :style="{top: `${100-percentage}%` }" />
+      <span class="fill" :style="{top: `${90-percentage}%` }" />
       <span class="score">
         {{ percentage }}%
         <div style="font-size: 0.8rem">Match</div>
@@ -42,29 +42,39 @@ export default {
     justify-content: center;
     text-align: center;
   }
-  .badge .level .fill {
+  .badge .level .fill,
+  .badge .level .fill::before,
+  .badge .level .fill::after {
     position: absolute;
     bottom: -1px;
     left: -1px;
     right: -1px;
+
+    background-repeat: repeat-x;
+  } 
+  .badge .level .fill {
     top: 45%;
+
+    background: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="isolation:isolate" viewBox="555 349 100 161.348" width="100" height="161.348"><path d=" M 655 510.348 L 655 510.348 L 555 510.348 L 555 349 C 581.484 349 587.192 365.21 603.402 365.21 C 619.612 365.21 634.959 349 655 349 L 655 510.348 Z " fill="rgb(0,0,0)"/></svg>');
+    animation: flow 10s linear infinite;
   }
   .badge .level .fill::before,
   .badge .level .fill::after {
     content: '';
-    position: absolute;
-    top: -3px;
-    opacity: 0.5;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    background-color: inherit;
-    transform: rotate(-1deg);
+    top: -1px;
+  }
+
+  .badge .level .fill::before {
+    opacity: 0.6;
+    background: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="isolation:isolate" viewBox="555 349 100 161.348" width="100" height="161.348"><path d=" M 655 510.348 L 655 510.348 L 555 510.348 L 555 349 C 581.484 349 587.192 365.21 603.402 365.21 C 619.612 365.21 634.959 349 655 349 L 655 510.348 Z " fill="rgb(0,0,0)"/></svg>');
+    background-position: 80px 10px;
+    animation: flow 13s linear infinite;
   }
   .badge .level .fill::after {
-    top: -7px;
-    opacity: 0.2;
-    transform: rotate(-3deg);
+    opacity: 0.4;
+    background: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="isolation:isolate" viewBox="555 349 100 161.348" width="100" height="161.348"><path d=" M 655 510.348 L 655 510.348 L 555 510.348 L 555 349 C 581.484 349 587.192 365.21 603.402 365.21 C 619.612 365.21 634.959 349 655 349 L 655 510.348 Z " fill="rgb(0,0,0)"/></svg>');
+    background-position: 30px 15px;
+    animation: flow 15s linear infinite;
   }
 
   .badge .level .score {
@@ -82,27 +92,39 @@ export default {
   .badge.fake .level {
     border-color: #ee4d45;
   }
-  .badge.fake .level .fill {
-    background-color: #ee4d45;
+  .badge.fake .level .fill,
+  .badge.fake .level .fill::before,
+  .badge.fake .level .fill::after {
+    background: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="isolation:isolate" viewBox="555 349 100 161.348" width="100" height="161.348"><path d=" M 655 510.348 L 655 510.348 L 555 510.348 L 555 349 C 581.484 349 587.192 365.21 603.402 365.21 C 619.612 365.21 634.959 349 655 349 L 655 510.348 Z " fill="#ee4d45"/></svg>');
   }
 
   .badge.real .level {
     border-color: #1DD53F;
   }
-  .badge.real .level .fill {
-    background-color: #1DD53F;
+  .badge.real .level .fill,
+  .badge.real .level .fill::before,
+  .badge.real .level .fill::after {
+    background: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="isolation:isolate" viewBox="555 349 100 161.348" width="100" height="161.348"><path d=" M 655 510.348 L 655 510.348 L 555 510.348 L 555 349 C 581.484 349 587.192 365.21 603.402 365.21 C 619.612 365.21 634.959 349 655 349 L 655 510.348 Z " fill="#1DD53F"/></svg>');
   }
 
   .badge.warn .level {
     border-color: #F45520;
   }
-  .badge.warn .level .fill {
-    background-color: #F45520;
+  .badge.warn .level .fill,
+  .badge.warn .level .fill::before,
+  .badge.warn .level .fill::after {
+    background: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="isolation:isolate" viewBox="555 349 100 161.348" width="100" height="161.348"><path d=" M 655 510.348 L 655 510.348 L 555 510.348 L 555 349 C 581.484 349 587.192 365.21 603.402 365.21 C 619.612 365.21 634.959 349 655 349 L 655 510.348 Z " fill="#F45520"/></svg>');
   }
 
   @media screen and (min-width: 950px) {
     .badge {
       width: 95%;
+    }
+  }
+
+  @keyframes flow {
+    0% {
+      background-position-x: -500px;
     }
   }
 </style>
